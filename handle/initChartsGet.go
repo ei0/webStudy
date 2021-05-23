7687dc1c06a167435e330a3785dc917dd99db904
ei0 <48590828+ei0@users.noreply.github.com>
2021-05-23 17:13:30 +0800

    排行榜完善

--- handle/initChartsGet.go
+++ handle/initChartsGet.go
@@ -13,8 +13,12 @@ func InitChartsGet(c *gin.Context) {
 	//点赞数，用户名，动态简单信息
 	const LIMIT int = 10
 	var likes [LIMIT]int
-	var nicknames [LIMIT]string
-	var messages [LIMIT]string
+	var nicknamesLike [LIMIT]string
+	var messagesLike [LIMIT]string
+	var collects [LIMIT]int
+	var nicknamesCollect [LIMIT]string
+	var messagesCollect [LIMIT]string
+	var collectT []sql.Message
 	var likeT []sql.Message
 	//sql.SDB.Table("messages").Order("`like`").Limit(10).Find(&likeT)
 	sql.SDB.Debug().Raw("SELECT * FROM test.messages order by `like` desc").Limit(10).Scan(&likeT)
@@ -23,27 +27,49 @@ func InitChartsGet(c *gin.Context) {
 		//fmt.Println(SubstrByByte(v.Text, 50))
 		src := []rune(v.Text)
 		if len(src) < 10 {
-			messages[k] = string(src[:len(src)])
+			messagesLike[k] = string(src[:len(src)])
 			//SubstrByByte(v.Like,10)
 		} else {
-			messages[k] = string(src[:10])
+			messagesLike[k] = string(src[:10])
 		}
 		var user sql.User
 		sql.SDB.Where("id=?", v.Uid).First(&user)
-		nicknames[k] = user.Name
+		nicknamesLike[k] = user.Name
 
 	}
 	fmt.Println("点赞数组", likes)
-	fmt.Println("用户名数组", nicknames)
-	fmt.Println("消息数组", messages)
+	fmt.Println("用户名数组", nicknamesLike)
+	fmt.Println("消息数组", messagesLike)
+	sql.SDB.Debug().Raw("SELECT * FROM test.messages order by `collect` desc").Limit(10).Scan(&collectT)
+	for k, v := range collectT {
+		collects[k] = v.Collect
+		//fmt.Println(SubstrByByte(v.Text, 50))
+		src := []rune(v.Text)
+		if len(src) < 10 {
+			messagesCollect[k] = string(src[:len(src)])
+			//SubstrByByte(v.Like,10)
+		} else {
+			messagesCollect[k] = string(src[:10])
+		}
+		var user sql.User
+		sql.SDB.Where("id=?", v.Uid).First(&user)
+		nicknamesCollect[k] = user.Name
+
+	}
+	fmt.Println("收藏数组", collects)
+	fmt.Println("用户名数组", nicknamesCollect)
+	fmt.Println("消息数组", messagesCollect)
 	// c.JSON(200, gin.H{
 	// 	"status": true,
 	// })
 	c.JSON(200, gin.H{
-		"status":    true,
-		"likes":     likes,
-		"nicknames": nicknames,
-		"messages":  messages,
+		"status":           true,
+		"likes":            likes,
+		"nicknamesLike":    nicknamesLike,
+		"messagesLike":     messagesLike,
+		"collects":         collects,
+		"nicknamescollect": nicknamesCollect,
+		"messagescollect":  messagesCollect,
 	})
 
 }
